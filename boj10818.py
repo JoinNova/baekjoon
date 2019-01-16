caac6e9f250d6dbc5610945e5237e0fb88671939
YeonhoShin <43157214+JoinNova@users.noreply.github.com>
2019-01-17 00:21:03 +0900

    Add files via upload

---
+++ boj10818.py
@@ -0,0 +1,6 @@
+#10818최소 최대
+input();l=list(map(int,input().split()));print(min(l),max(l))
+#02
+input();l=sorted(map(int,input().split()));print(l[0],l[-1])
+#03
+input();*m,=map(int,input().split());print(min(m),max(m))
