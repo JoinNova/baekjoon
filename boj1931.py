ae3050d8f6552ab522f6a57c2dd4720d2f1dff82
YeonhoShin <43157214+JoinNova@users.noreply.github.com>
2019-02-11 20:32:54 +0900

    Update boj1931.py

--- boj1931.py
+++ boj1931.py
@@ -3,3 +3,17 @@ n=int(input());l=sorted([*map(int,input().split()[::-1])]for _ in'_'*n);f=0;r=0
 for _ in l:
  if _[1]>=f:r+=1;f=_[0]
 print(r)
+
+#02
+n=int(input())
+conference=sorted([*map(int,input().split()[::-1])]for _ in'_'*n)
+                                     #we have to know conference's endtime
+
+end=0
+result=0
+
+for start in conference:
+    if start[1]>=end:
+        result+=1
+        end=_[0]
+print(result)
