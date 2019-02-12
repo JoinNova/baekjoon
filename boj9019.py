f8a46f10874994d67a0523d07f69e6f85a3d12a6
YeonhoShin <43157214+JoinNova@users.noreply.github.com>
2019-02-13 01:02:28 +0900

    Update boj9019.py

--- boj9019.py
+++ boj9019.py
@@ -127,3 +127,36 @@ for _ in'_'*int(put()):
     a,b=map(int,put().split());q=que();v=[]
     q.append([a,'']);v.append(a)
     print(bfs(b,q,v))
+    
+###pypy3    
+from collections import deque
+def D(k):
+    return (2*k)%10000,'D'
+def S(k):
+    if k==0:
+        return 9999,'S'
+    else:
+        return k-1,'S'
+def L(k):
+    return (k%1000)*10+(k//1000),'L'
+def R(k):
+    return (k//10)+(k%10)*1000,'R'
+ 
+for __ in range(int(input())):
+    n,m=map(int,input().split())
+    A,F=['']*10000,[-1]*10000
+    q=deque()
+    q.append(n)
+    F[n]=-2
+    while q:
+        x=q.popleft()
+        for i,j in [D(x),S(x),L(x),R(x)]:
+            if F[i]==-1:
+                q.append(i)
+                A[i]=j
+                F[i]=x
+    k=''
+    while m!=n:
+        k+=A[m]
+        m=F[m]
+    print(k[::-1])
