bae1951a4a1558daaaa1c42a8735354eac8be89a
YeonhoShin <43157214+JoinNova@users.noreply.github.com>
2019-02-19 17:36:05 +0900

    Create boj15654.py

---
+++ boj15654.py
@@ -0,0 +1,6 @@
+#boj15654 N&M(6)
+'''from itertools import*;n,m=map(int,input()[::2])
+for _ in combinations(sorted(input().split(),key=int),m):print(*_)'''
+
+#02
+[m,n],l=eval('map(int,input().split()),'*2);from itertools import*;*starmap(print,combinations(sorted(l),n)),
