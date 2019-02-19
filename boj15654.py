e3faf8a6b5ef580fc5171b707e1900d5d2ceeff9
YeonhoShin <43157214+JoinNova@users.noreply.github.com>
2019-02-19 17:56:14 +0900

    Update boj15654.py

--- boj15654.py
+++ boj15654.py
@@ -1,6 +1,3 @@
-#boj15654 N&M(6)
+#boj15654 N&M(5)
 from itertools import*;n,m=map(int,input()[::2])
 for _ in permutations(sorted(map(int,input().split())),m):print(*_)
-
-#02
-m,n=map(int,input().split());from itertools import*;*starmap(print,permutations(sorted(input().split(),key=int),n)),
