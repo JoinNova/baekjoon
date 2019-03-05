dc2b4353262fe3d44f1104e194faf4d36df66630
YeonhoShin <43157214+JoinNova@users.noreply.github.com>
2019-03-05 15:45:09 +0900

    Update boj14863.py

--- boj14863.py
+++ boj14863.py
@@ -15,3 +15,42 @@ for i in range(1,n+1):
         if dp[n][i]>result:
             result=dp[n][i]
 print(result)
+
+
+
+#by ftstokpark (Fixed by mulijoa)
+import sys
+
+N, K = map(int, sys.stdin.readline().split())
+
+# First
+pre_d = dict()
+walk_time, walk_money, bike_time, bike_money = map(int, sys.stdin.readline().split())
+if walk_time==bike_time :
+    if walk_money > bike_money :
+        pre_d[walk_time] = walk_money
+    else :
+        pre_d[walk_time] = bike_money
+else :
+    pre_d[bike_time] = bike_money
+    pre_d[walk_time] = walk_money
+    
+for __ in range(N - 1):
+    d = dict()
+    walk_time, walk_money, bike_time, bike_money = map(int, sys.stdin.readline().split())
+
+    for k in pre_d.keys():
+        if k + walk_time <= K:
+            if k + walk_time in d:
+                d[k + walk_time] = max(d[k + walk_time], pre_d[k] + walk_money)
+            else:
+                d[k + walk_time] = pre_d[k] + walk_money
+
+        if k + bike_time <= K:
+            if k + bike_time in d:
+                d[k + bike_time] = max(d[k + bike_time], pre_d[k] + bike_money)
+            else:
+                d[k + bike_time] = pre_d[k] + bike_money
+    pre_d = d
+
+print(max(pre_d.values()))
