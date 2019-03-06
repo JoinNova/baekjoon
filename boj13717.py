6ad118be301c8b31927a5f9805b0ebb15309d12b
YeonhoShin <43157214+JoinNova@users.noreply.github.com>
2019-03-06 21:21:55 +0900

    Update boj13717.py

--- boj13717.py
+++ boj13717.py
@@ -13,3 +13,12 @@ for _ in'_'*int(input()):
     if result[0]<evolution:
         result=[evolution,pokemon]
 print(total,result[1],sep='\n')
+
+
+#02
+r=t=0;i=input
+for _ in'_'*int(i()):
+ e=0;p=i();k,m=map(int,i().split())
+ while m-k>=0:m-=k;e+=1;m+=2;t+=1
+ if r<e:r=e;n=p
+print(t,n,sep='\n')
