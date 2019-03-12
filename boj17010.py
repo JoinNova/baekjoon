9724b32f1c916a8e4dfca80985cc7052ac314817
YeonhoShin <43157214+JoinNova@users.noreply.github.com>
2019-03-12 21:21:54 +0900

    Create boj17010.py

---
+++ boj17010.py
@@ -0,0 +1,6 @@
+#boj17010 Time to Decompress
+for _ in'_'*int(input()):a=input();print(int(a[:-1])*a[-1])
+#02
+exec('a=input();print(int(a[:2])*a[-1]);'*int(input()))
+#03
+i=input;exec('a=i();print(int(a[:2])*a[-1]);'*int(i()))
