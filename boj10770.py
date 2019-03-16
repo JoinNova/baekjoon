3a1403a47cb91dd11e85cbdb69f2a782909305b0
YeonhoShin <43157214+JoinNova@users.noreply.github.com>
2019-03-16 22:07:24 +0900

    Create boj10770.py

---
+++ boj10770.py
@@ -0,0 +1,4 @@
+#boj10770 Rövarspråket
+convert=['a','bac','cad','def','e','feg','geh','hij','i','jik','kil',
+         'lim','mon','nop','o','poq','qor','ros','sut','tuv','u','vuw','wux','xuy','yuz','zuz']
+for _ in input():print(convert[ord(_)-97],end='')
