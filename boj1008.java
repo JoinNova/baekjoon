04aa907dd95a82f9112b3ebe383afce291deabe7
YeonhoShin <43157214+JoinNova@users.noreply.github.com>
2019-03-18 14:16:02 +0900

    Create boj1008.java

---
+++ boj1008.java
@@ -0,0 +1,9 @@
+//boj1008 A/B
+import java.util.*;
+
+public class Main {
+	public static void main(String args[]){
+		Scanner sc =  new Scanner(System.in);
+		System.out.println(sc.nextDouble() / sc.nextDouble());
+	}
+}
