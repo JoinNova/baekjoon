01b39cea660c89a3fc72b37f3418847d639a0721
YeonhoShin <43157214+JoinNova@users.noreply.github.com>
2019-03-18 14:04:48 +0900

    Create boj1001.java

---
+++ boj1001.java
@@ -0,0 +1,9 @@
+//boj1001 A-B
+import java.util.Scanner;
+
+class Main {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println( sc.nextInt() - sc.nextInt() );
+	}
+}
