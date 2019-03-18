05fb7deab66893afd2ff65fd3f94fbedff50a1f8
YeonhoShin <43157214+JoinNova@users.noreply.github.com>
2019-03-18 14:15:15 +0900

    Create boj10998.java

---
+++ boj10998.java
@@ -0,0 +1,9 @@
+//boj10998 AxB
+import java.util.Scanner;
+
+class Main {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println( sc.nextInt() * sc.nextInt() );
+	}
+}
