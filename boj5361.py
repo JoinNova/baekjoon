b1b91c8b1135ded57f96186dc5bbfb50a14dcccc
YeonhoShin <43157214+JoinNova@users.noreply.github.com>
2019-03-27 10:02:31 +0900

    Create boj5361.py

---
+++ boj5361.py
@@ -0,0 +1,7 @@
+#boj5361 전투드루이드가격
+exec('a,b,c,d,e=map(int,input().split());print("$%.2f"%(350.34*a+230.9*b+190.55*c+d*125.3+180.9*e));'*int(input()))
+
+#02
+for _ in'_'*int(input()):
+ a,b,c,d,e=map(int,input().split())
+ print('$%.2f'%(350.34*a+230.90*b+190.55*c+d*125.30+180.90*e))
