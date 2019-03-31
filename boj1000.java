defde1c4db79a3a44f13436259aded04628d5f6a
YeonhoShin <43157214+JoinNova@users.noreply.github.com>
2019-03-31 11:22:56 +0900

    Update boj1000.java

--- boj1000.java
+++ boj1000.java
@@ -4,6 +4,6 @@ import java.util.Scanner;
 class Main {
 	public static void main(String args[]) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println( sc.nextInt() + sc.nextInt() );
+		System.out.println( sc.nextInt() + sc.nextInt());
 	}
 }
