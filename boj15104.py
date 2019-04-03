7076b26f625d13ad0100bcd0dc4c46df22e097ec
YeonhoShin <43157214+JoinNova@users.noreply.github.com>
2019-04-03 14:49:44 +0900

    Update boj15104.py

--- boj15104.py
+++ boj15104.py
@@ -1,2 +1,2 @@
 #boj15104 Odd Palindrome
-print(['Or not.','Odd.'][len(input())%2])
+t=input();print(['Or not.','Odd.'][all([_!=_[::-1]for _ in[t[i:j]for i in range(len(s))for j in range(i+2,len(t)+1,2)]])])
